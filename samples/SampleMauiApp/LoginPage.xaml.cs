cdaefd03463def3233a386ff6f056f8fea33d65a
redth <jondick@gmail.com>
2022-09-21 19:42:22 -0400

    net7, add login page

---
+++ samples/SampleMauiApp/LoginPage.xaml.cs
@@ -0,0 +1,14 @@
+namespace SampleMauiApp;
+
+public partial class LoginPage : ContentPage
+{
+	public LoginPage()
+	{
+		InitializeComponent();
+	}
+
+	private void Button_Clicked(object sender, EventArgs e)
+	{
+
+	}
+}
