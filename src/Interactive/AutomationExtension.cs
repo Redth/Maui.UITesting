e3cd1b61f2eb6891cdd2d3db2c5fd303ccaec3eb
Diego Colombo <dicolomb@microsoft.com>
2022-10-07 15:10:44 +0100

    use magic command

--- src/Interactive/AutomationExtension.cs
+++ src/Interactive/AutomationExtension.cs
@@ -1,87 +1,129 @@
 ﻿using System.CommandLine;
 using System.CommandLine.Binding;
 using Microsoft.DotNet.Interactive;
+using Microsoft.DotNet.Interactive.CSharp;
+using Microsoft.Maui.Automation.Driver;
 
 namespace Microsoft.Maui.Automation.Interactive;
 
 public class AutomationExtensions : IKernelExtension
 {
-	Driver.AppDriver? driver;
+	
 
 	public Task OnLoadAsync(Kernel kernel)
 	{
+        if (kernel is CSharpKernel csharpKernel)
+        {
+            var platformOption = new Option<Platform>(
+                new[] {"--platform"},
+                "The device platform to test on.")
+            {
+                Arity = ArgumentArity.ExactlyOne
+            };
+            var appIdOption = new Option<string>(
+                new[] {"--app-id"},
+                "The application id (bundle id, package id, etc) to test.")
+            {
+                Arity = ArgumentArity.ExactlyOne
+            };
+            var appOption = new Option<string>(
+                new[] {"--app"},
+                "The application file (.app, .apk, etc.) to test.")
+            {
+                Arity = ArgumentArity.ExactlyOne
+            };
+            var automationPlatformOption = new Option<Platform>(
+                new[] {"--automation-platform"},
+                "The automation platform to interact with, if different than the device platform (ie: Maui).")
+            {
+                Arity = ArgumentArity.ZeroOrOne
+            };
+            var deviceOption = new Option<string>(
+                new[] {"--device"},
+                getDefaultValue: () => string.Empty,
+                description:
+                "Device ID to test on (eg: ADB Serial for an android device, UDID of emulator for iOS/tvOS/ipadOS).")
+            {
+                Arity = ArgumentArity.ZeroOrOne
+            };
 
-		var platformOption = new Option<Platform>(
-			new[] { "--platform" },
-			"The device platform to test on.")
-		{
-			Arity = ArgumentArity.ExactlyOne
-		};
-		var appIdOption = new Option<string>(
-			new[] { "--app-id" },
-			"The application id (bundle id, package id, etc) to test.")
-		{
-			Arity = ArgumentArity.ExactlyOne
-		};
-		var appOption = new Option<string>(
-			new[] { "--app" },
-			"The application file (.app, .apk, etc.) to test.")
-		{
-			Arity = ArgumentArity.ExactlyOne
-		};
-		var automationPlatformOption = new Option<Platform>(
-			new[] { "--automation-platform" },
-			"The automation platform to interact with, if different than the device platform (ie: Maui).")
-		{
-			Arity = ArgumentArity.ZeroOrOne
-		};
-		var deviceOption = new Option<string>(
-			new[] { "--device" },
-			getDefaultValue: () => string.Empty,
-			description: "Device ID to test on (eg: ADB Serial for an android device, UDID of emulator for iOS/tvOS/ipadOS).")
-		{
-			Arity = ArgumentArity.ZeroOrOne
-		};
+            
+            deviceOption.AddCompletions((context) =>
+            {
+                // get list of valid devices string and return it
+                return Array.Empty<string>();
+            });
 
 
-		var testOption = new Command("#!uitest", "Starts a UI Testing session")
-		{
-			platformOption,
-			automationPlatformOption,
-			appIdOption,
-			appOption,
-			deviceOption
-		};
+            var nameOption = new Option<string>(
+                new[] { "--name" },
+                getDefaultValue: () => "device",
+                description:
+                "Name for the local variable.")
+            {
+                Arity = ArgumentArity.ZeroOrOne
+            };
 
-		testOption.SetHandler(
-			async (Platform platform, Platform automationPlatform, string appId, string app, string device) =>
-			{
 
-				if (driver is not null)
-				{
-					await driver.StopApp();
-					await driver.ClearAppState();
-					driver.Dispose();
-					driver = null;
-				}
+            var testOption = new Command("#!uitest", "Starts a UI Testing session")
+            {
+                platformOption,
+                automationPlatformOption,
+                appIdOption,
+                appOption,
+                deviceOption,
+                nameOption
+            };
 
-				driver = new Driver.AppDriver(
-					new Driver.AutomationConfiguration(
-						appId, app, platform, device, automationPlatform));
+            testOption.SetHandler(
+                async (Platform platform, Platform automationPlatform, string appId, string app, string device, string name) =>
+                {
+                    // if there is an already used dirver with such name dispose
+                    if (csharpKernel.TryGetValue(name, out AppDriver oldDriver) && oldDriver is {})
+                    {
+                        await DisposeAppDriver(oldDriver);
+                    }
 
-				await driver.InstallApp();
+                    var driver = new AppDriver(
+                        new AutomationConfiguration(
+                            appId, app, platform, device, automationPlatform));
 
-				//KernelInvocationContext.Current.Display(SvgClock.DrawSvgClock(hour, minute, second));
-			},
-			platformOption,
-			automationPlatformOption,
-			appIdOption,
-			appOption,
-			deviceOption);
+                    csharpKernel.RegisterForDisposal(async() =>
+                    {
+                        await DisposeAppDriver(driver);
+                    });
 
-		kernel.AddDirective(testOption);
+                    await driver.InstallApp();
 
-		return Task.CompletedTask;
-	}
+                    await csharpKernel.SetValueAsync(name, driver);
+
+                    //KernelInvocationContext.Current.Display(SvgClock.DrawSvgClock(hour, minute, second));
+                },
+                platformOption,
+                automationPlatformOption,
+                appIdOption,
+                appOption,
+                deviceOption,
+                nameOption);
+
+            csharpKernel.AddDirective(testOption);
+        }
+
+        return Task.CompletedTask;
+
+        async Task DisposeAppDriver(AppDriver driver)
+        {
+            try
+            {
+                await driver.StopApp();
+                await driver.ClearAppState();
+                driver.Dispose();
+            }
+            catch
+            {
+
+            }
+        }
+    }
 }
 
