d6fcd8d29d0f9b62db6ffeb4d73a7092f0c6cdd6
redth <jondick@gmail.com>
2022-10-14 11:36:05 -0400

    Load on all possible kernels, better device loading

--- src/Interactive/AutomationExtension.cs
+++ src/Interactive/AutomationExtension.cs
@@ -7,101 +7,118 @@ using AndroidSdk;
 using Microsoft.Maui.Automation.Util;
 using Microsoft.DotNet.Interactive.Formatting;
 using static Microsoft.DotNet.Interactive.Formatting.PocketViewTags;
+using System.Runtime.InteropServices;
+using System.Collections.Concurrent;
+using System.Collections.ObjectModel;
 
 namespace Microsoft.Maui.Automation.Interactive;
 
 public class AutomationExtensions : IKernelExtension
 {
-	public Task OnLoadAsync(Kernel kernel)
+	TaskCompletionSource<IDictionary<string, List<string>>> tcsDevices = new();
+	public Task<IDictionary<string, List<string>>> Devices
+		=> tcsDevices.Task;
+
+	public async Task OnLoadAsync(Kernel kernel)
 	{
-		if (kernel is CSharpKernel csharpKernel)
+		var startedLoadingDevices = false;
+
+		var installedOnKernels = new List<string>();
+		var candidateKernels = kernel.FindKernels(k => k is CSharpKernel).Cast<CSharpKernel>();
+		foreach (var csKernel in candidateKernels)
 		{
-			var platformOption = new Option<Platform>(
+			if (csKernel.Directives.Any(d => d.Name.Equals("#!uitest", StringComparison.OrdinalIgnoreCase)))
+			{
+				if (!startedLoadingDevices)
+				{
+					startedLoadingDevices = true;
+					var _ = LoadDevices().ContinueWith(t => tcsDevices.TrySetResult(t.Result));
+				}
+
+				await ConfigureKernelDirective(csKernel);
+				installedOnKernels.Add(csKernel.Name);
+			}
+		}
+
+		if (installedOnKernels.Count > 0 && KernelInvocationContext.Current is { } context)
+		{
+			PocketView view = div(
+				code("Maui.UITesting"),
+				$" is loaded. It adds commands for UI Testing/Automation.  Available on {string.Join(", ", installedOnKernels)}.",
+				code("#!uitest -h")
+			);
+
+			context.Display(view);
+		}
+
+	}
+
+	async Task ConfigureKernelDirective(CSharpKernel csharpKernel)
+	{
+		var platformOption = new Option<Platform>(
 				new[] { "--platform" },
 				() => Platform.Maui,
 				"The device platform to test on.")
-			{
-				Arity = ArgumentArity.ExactlyOne
-			};
-			platformOption.AddCompletions("Android", "iOS", "MacCatalsyt", "tvOS", "WinAppSDK");
+		{
+			Arity = ArgumentArity.ExactlyOne
+		};
+		platformOption.AddCompletions("Android", "iOS", "MacCatalsyt", "tvOS", "WinAppSDK");
 
-			var appIdOption = new Option<string>(
-				new[] { "--app-id" },
-				"The application id (bundle id, package id, etc) to test.")
-			{
-				Arity = ArgumentArity.ZeroOrOne
-			};
+		var appIdOption = new Option<string>(
+			new[] { "--app-id" },
+			"The application id (bundle id, package id, etc) to test.")
+		{
+			Arity = ArgumentArity.ZeroOrOne
+		};
 
-			var appOption = new Option<FileInfo>(
-				new[] { "--app" },
-				"The application file (.app, .apk, etc.) to test.")
-			{
-				Arity = ArgumentArity.ExactlyOne
-			};
-			var automationPlatformOption = new Option<Platform>(
-				new[] { "--automation-platform" },
-				() => Platform.Maui,
-				"The automation platform to interact with, if different than the device platform (ie: Maui).")
-			{
-				Arity = ArgumentArity.ZeroOrOne
-			};
-			var deviceOption = new Option<string>(
-				new[] { "--device" },
-				getDefaultValue: () => string.Empty,
-				description:
-				"Device ID to test on (eg: ADB Serial for an android device, UDID of emulator for iOS/tvOS/ipadOS).")
-			{
-				Arity = ArgumentArity.ZeroOrOne
-			};
+		var appOption = new Option<FileInfo?>(
+			new[] { "--app" },
+			"The application file (.app, .apk, etc.) to test.")
+		{
+			Arity = ArgumentArity.ZeroOrOne
+		};
+		var automationPlatformOption = new Option<Platform>(
+			new[] { "--automation-platform" },
+			() => Platform.Maui,
+			"The automation platform to interact with, if different than the device platform (ie: Maui).")
+		{
+			Arity = ArgumentArity.ZeroOrOne
+		};
+		var deviceOption = new Option<string>(
+			new[] { "--device" },
+			getDefaultValue: () => string.Empty,
+			description:
+			"Device ID to test on (eg: ADB Serial for an android device, UDID of emulator for iOS/tvOS/ipadOS).")
+		{
+			Arity = ArgumentArity.ZeroOrOne
+		};
 
-			deviceOption.AddCompletions((context) =>
+
+		var allDevices = await Devices;
+
+		deviceOption.AddCompletions(context =>
+			context.ParseResult.GetValueForOption(automationPlatformOption) switch
 			{
-				var platform = context.ParseResult.GetValueForOption(automationPlatformOption);
-				var devices = new List<string>();
-				switch (platform)
-				{
-					case Platform.Maui:
-						AddAndroidDevices(devices);
-						AddAppleDevices("ios", devices);
-						AddAppleDevices("macos", devices);
-						AddAppleDevices("tvos", devices);
-						devices.Add("Windows");
-						break;
-					case Platform.Ios:
-						AddAppleDevices("ios", devices);
-						break;
-					case Platform.Maccatalyst:
-					case Platform.Macos:
-						AddAppleDevices("macos", devices);
-						break;
-					case Platform.Tvos:
-						AddAppleDevices("tvos", devices);
-						break;
-					case Platform.Android:
-						AddAndroidDevices(devices);
-						break;
-					case Platform.Winappsdk:
-						devices.Add("Windows");
-						break;
-					default:
-						throw new ArgumentOutOfRangeException();
-				}
-				// get list of valid devices string and return it
-				return devices;
+				Platform.Maui => allDevices.Values.SelectMany(v => v),
+				Platform.Ios => allDevices["ios"],
+				Platform.Tvos => allDevices["tvos"],
+				Platform.Maccatalyst => allDevices["macos"],
+				Platform.Macos => allDevices["macos"],
+				Platform.Winappsdk => allDevices["windows"],
+				_ => Enumerable.Empty<string>()
 			});
 
+		var nameOption = new Option<string>(
+			new[] { "--name" },
+			getDefaultValue: () => "Driver",
+			description:
+			"Name for the local variable.")
+		{
+			Arity = ArgumentArity.ZeroOrOne
+		};
 
-			var nameOption = new Option<string>(
-				new[] { "--name" },
-				getDefaultValue: () => "driver",
-				description:
-				"Name for the local variable.")
-			{
-				Arity = ArgumentArity.ZeroOrOne
-			};
 
-
-			var testCommand = new Command("#!uitest", "Starts a UI Testing Driver session")
+		var testCommand = new Command("#!uitest", "Starts a UI Testing Driver session")
 			{
 				platformOption,
 				automationPlatformOption,
@@ -111,53 +128,48 @@ public class AutomationExtensions : IKernelExtension
 				nameOption
 			};
 
-			testCommand.SetHandler<Platform, Platform, string, FileInfo, string, string>(
-				async (Platform platform, Platform automationPlatform, string appId, FileInfo app, string device, string name) =>
-				{
-					// if there is an already used dirver with such name dispose
-					if (csharpKernel.TryGetValue(name, out AppDriver oldDriver) && oldDriver is { })
-					{
-						await DisposeAppDriver(oldDriver);
-					}
-
-					var driver = new AppDriver(
-						new AutomationConfiguration(
-							appId, app, platform, device, automationPlatform));
-
-					csharpKernel.RegisterForDisposal(async () =>
-					{
-						await DisposeAppDriver(driver);
-					});
-
-					await driver.Start();
-
-					await csharpKernel.SetValueAsync(name, driver);
-				},
-				platformOption,
-				automationPlatformOption,
-				appIdOption,
-				appOption,
-				deviceOption,
-				nameOption);
-
-			csharpKernel.AddDirective(testCommand);
-
-			if (KernelInvocationContext.Current is { } context)
+		testCommand.SetHandler<Platform, Platform, string, FileInfo?, string, string>(
+			async (Platform platform, Platform automationPlatform, string appId, FileInfo? app, string device, string name) =>
 			{
-				PocketView view = div(
-					code("Maui.UITesting"),
-					" is loaded. It adds commands for UI Testing/Automation.",
-					code("#!uitest -h")
-				);
+				// if there is an already used dirver with such name dispose
+				if (csharpKernel.TryGetValue(name, out IDriver oldDriver) && oldDriver is { })
+				{
+					await DisposeAppDriver(oldDriver);
+				}
 
-				context.Display(view);
-			}
-		}
+				var builder = new AppDriverBuilder()
+					.DevicePlatform(platform)
+					.AutomationPlatform(automationPlatform);
 
-		return Task.CompletedTask;
+				if (app is not null)
+					builder = builder.AppFilename(app.FullName);
+				if (!string.IsNullOrEmpty(appId))
+					builder = builder.AppId(appId);
+				if (!string.IsNullOrEmpty(device))
+					builder = builder.Device(device);
+
+				var driver = builder.Build();
+
+				csharpKernel.RegisterForDisposal(async () =>
+				{
+					await DisposeAppDriver(driver);
+				});
+
+				await driver.Start();
+
+				await csharpKernel.SetValueAsync(name, driver);
+			},
+			platformOption,
+			automationPlatformOption,
+			appIdOption,
+			appOption,
+			deviceOption,
+			nameOption);
+
+		csharpKernel.AddDirective(testCommand);
 	}
 
-	Task DisposeAppDriver(AppDriver driver)
+	Task DisposeAppDriver(IDriver driver)
 	{
 		try
 		{
@@ -169,22 +181,79 @@ public class AutomationExtensions : IKernelExtension
 		return Task.CompletedTask;
 	}
 
-	void AddAndroidDevices(List<string> devices)
+	async Task<IDictionary<string, List<string>>> LoadDevices()
 	{
-		var adb = new Adb();
-		var adbDevices = adb.GetDevices();
-		devices.AddRange(adbDevices.Select(d => d.Serial));
+		var devices = new ConcurrentDictionary<string, List<string>>();
+		devices["android"] = new();
 
-		var avd = new AvdManager();
-		var avds = avd.ListAvds();
-		devices.AddRange(avds.Select(a => a.Name));
+		if (RuntimeInformation.IsOSPlatform(OSPlatform.OSX))
+		{
+			devices["ios"] = new();
+			devices["tvos"] = new();
+			devices["macos"] = new();
+		}
+		else if (RuntimeInformation.IsOSPlatform(OSPlatform.Windows))
+		{
+			devices["windows"] = new() { "Windows" };
+		}
+
+		var deviceTasks = new List<Task>();
+		deviceTasks.Add(Task.Run(() => AddAndroidDevices(devices["android"])));
+
+		if (RuntimeInformation.IsOSPlatform(OSPlatform.OSX))
+		{
+			deviceTasks.Add(Task.Run(() => AddAppleDevices("ios", devices["ios"])));
+			deviceTasks.Add(Task.Run(() => AddAppleDevices("tvos", devices["tvos"])));
+			deviceTasks.Add(Task.Run(() => AddAppleDevices("macos", devices["macos"])));
+		}
+
+		await Task.WhenAll(deviceTasks).ConfigureAwait(false);
+
+		return devices;
 	}
 
-	void AddAppleDevices(string platform, List<string> devices)
+	void AddAndroidDevices(IList<string> devices)
 	{
-		var appleDevices = Xcode.GetDevices(platform).Select(d => d.Name);
-		if (appleDevices.Any())
-			devices.AddRange(appleDevices);
+		try
+		{
+			var adb = new Adb();
+			var adbDevices = adb.GetDevices();
+			foreach (var device in adbDevices)
+				devices.Add(device.Serial);
+		}
+		catch (Exception ex)
+		{
+			Console.Error.WriteLine(ex);
+		}
+
+		try
+		{
+			var avd = new AvdManager();
+			var avds = avd.ListAvds();
+			foreach (var device in avds)
+				devices.Add(device.Name);
+		}
+		catch (Exception ex)
+		{
+			Console.Error.WriteLine(ex);
+		}
+	}
+
+	void AddAppleDevices(string platform, IList<string> devices)
+	{
+		if (RuntimeInformation.IsOSPlatform(OSPlatform.OSX))
+		{
+			try
+			{
+				var appleDevices = Xcode.GetDevices(platform).Select(d => d.Name);
+				foreach (var device in appleDevices)
+					devices.Add(device);
+			}
+			catch (Exception ex)
+			{
+				Console.Error.WriteLine(ex);
+			}
+		}
 	}
 }
 
